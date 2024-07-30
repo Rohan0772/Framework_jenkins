173fa95fb2c4882c41091fb3f4e7e58e18d7db29
rohan <rohan91455@gmail.com>
2024-07-30 15:08:42 +0530

    third stage

--- JenkinsExecution/src/test/java/organisationtest/CreateOrganisationTest.java
+++ JenkinsExecution/src/test/java/organisationtest/CreateOrganisationTest.java
@@ -6,6 +6,15 @@ public class CreateOrganisationTest {
 	@Test
 	public void org1_Test() throws Exception {
 		System.out.println("SMOKE SUITE");
+		String URL = System.getProperty("url","default url");
+		String BROWSER = System.getProperty("browser","default browser");
+		String USERNAME = System.getProperty("username", "default un");
+		String PASSWORD = System.getProperty("password","default pw");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		
 	}
 
